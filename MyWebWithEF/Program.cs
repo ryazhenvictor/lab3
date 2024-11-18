4f00a4e3de2f6c23f90558ab3def9546a1e28169
Viktor <riazhen.viktor1124@vu.cdu.edu.ua>
2024-11-18 19:39:09 +0200

    commit and push

---
+++ MyWebWithEF/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+builder.Services.AddScoped<ProductService>();
+builder.Services.AddScoped<CategoryService>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "My API",
+        Version = "v1",
+        Description = "Документування API з використанням Swagger у .NET Core 8.0",
+        Contact = new OpenApiContact
+        {
+            Name = "Hnatyuk Dmytro",
+            Email = "example@example.com"
+        }
+    });
+});
+builder.Services.AddControllers();
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1");
+        c.RoutePrefix = string.Empty;
+    });
+}
+
+app.MapControllers();
+
+app.Run();
